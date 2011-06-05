b74b2e78c1632b5516ef5a7933ccbffda6e3ae5d
Ian Shafer <ian@raincityconsulting.com>
2011-06-05 14:18:36 -0700

    Make setup easier

---
+++ bin/setup-tomcat.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source $(dirname $0)/include.sh
+
+if test -z "$CATALINA_HOME"; then
+    fatal "CATALINA_HOME must be set"
+fi
+
+mkdir $BASEDIR/{webapps,temp,work,conf,logs}
+
+cp $CATALINA_HOME/conf/* $BASEDIR/conf
+
+mkdir -p $BASEDIR/conf/Catalina/localhost
+
+cp $CATALINA_HOME/webapps/manager/META-INF/context.xml $BASEDIR/conf/Catalina/localhost/manager.xml
