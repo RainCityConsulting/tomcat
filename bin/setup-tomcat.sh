cf20940f67351f25eb5faffa79cc2c3e052f67fb
Ian Shafer <ian@offandaway.com>
2011-06-27 15:08:22 -0700

    Fix manager context deploy

--- bin/setup-tomcat.sh
+++ bin/setup-tomcat.sh
@@ -9,7 +9,3 @@ fi
 mkdir $BASEDIR/{webapps,temp,work,logs}
 
 cp $CATALINA_HOME/conf/* $BASEDIR/conf
-
-mkdir -p $BASEDIR/conf/Catalina/localhost
-
-cp $CATALINA_HOME/webapps/manager/META-INF/context.xml $BASEDIR/conf/Catalina/localhost/manager.xml
