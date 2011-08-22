a6bca4b5389398091c316446e2a3bf666680b6de
Ian Shafer <ian@offandaway.com>
2011-08-22 09:45:38 -0700

    Add signal option to rekill

--- bin/rekill-tomcat.sh
+++ bin/rekill-tomcat.sh
@@ -2,7 +2,24 @@
 
 source $(dirname $0)/include.sh
 
-$BASEDIR/bin/kill-tomcat.sh
+signal=15
+
+while getopts "h9" option; do
+    case $option in
+        h ) print_usage ;;
+        9 ) signal=9 ;;
+        * ) print_usage "Unknown option: $option" ;;
+    esac
+done
+
+shift $(( $OPTIND - 1 ))
+
+if test $signal -eq 9; then
+  $BASEDIR/bin/kill-tomcat.sh -9
+else
+  $BASEDIR/bin/kill-tomcat.sh
+fi
+
 $BASEDIR/bin/wait-for-tomcat-to-stop.sh
 echo .
 $BASEDIR/bin/start-tomcat.sh
